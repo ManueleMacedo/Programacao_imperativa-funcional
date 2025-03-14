58dcf2bf0a89093258ee5207f589c4e1db67f4a0
Manuele Macêdo <manuelemacedo.ads@gmail.com>
2025-03-14 11:06:50 -0300

    atividades de C

---
+++ lista1_PIF/q2_mmps2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    int num1;
+    int num2;
+    
+    scanf("%d", &num1);
+    scanf("%d", &num2);
+    
+    int X = num1 + num2;
+    
+    printf("X = %d\n",X);
+    
+    return 0;
+}
---
+++ lista1_PIF/q4_mmps2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    int num1;
+    int num2;
+    
+    scanf("%d", &num1);
+    scanf("%d", &num2);
+    
+    int SOMA = num1 + num2;
+    
+    printf("SOMA = %d\n",SOMA);
+    
+    return 0;
+}
