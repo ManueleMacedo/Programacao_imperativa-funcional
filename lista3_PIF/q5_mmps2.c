43071af1783b9f035f139fcd239db4d85bd42777
Manuele Macêdo <manuelemacedo.ads@gmail.com>
2025-03-19 11:20:37 -0300

    inicio lista 3

---
+++ lista3_PIF/q5_mmps2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+    int N[20];
+    int valorOriginal;
+
+
+    for(int i = 0; i < 20; i++){
+        scanf("%d", &N[i]);
+    }
+
+    for(int i = 0; i <10; i++){
+            valorOriginal = N[i];
+            N[i] = N[19 -i];
+            N[19 -i] = valorOriginal;
+    }
+    
+    for(int i =0; i<20; i++){
+        printf("N[%d] = %d\n", i, N[i]);
+    }
+}
